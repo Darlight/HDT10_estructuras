a7c62e5dac20b4125fff5a86872af1417258fba2
LoboAnimae <30876166+LoboAnimae@users.noreply.github.com>
2019-05-16 17:45:11 -0600

    Update main.py

--- Clinic/main.py
+++ Clinic/main.py
@@ -4,7 +4,7 @@ Seccion 10 - Estructura de datos
 Grupo 6
 Mario Perdomo 18029
 Josue Sagastume 18173
-Andres Quan 17---
+Andres Quan 17652
 Main:
 Muestra las opciones necesarias para hacer ajuestes en la base de datos Neo4j
 """
