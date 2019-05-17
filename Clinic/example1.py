759af2d1c176707c76967c8111f30b674f8b915b
Darlight <andres.perdomo09@gmail.com>
2019-05-16 19:07:38 -0600

    Setting some DB functions

--- Clinic/example1.py
+++ Clinic/example1.py
@@ -13,4 +13,4 @@ patient = db.labels.create("Patient")
 b1 = db.nodes.create(name="Punk IPA")
 b2 = db.nodes.create(name="Hoegaarden Rosee")
 # You can associate a label with many nodes in one go
-beer.add(b1, b2)
+patient.add(b1, b2)
